4d0a61904c4fff7a02d77a1060c7076156cde343
dmazhartsev <den-nety@mail.ru>
2026-07-02 14:22:06 +0300

    chore: initial project structure

--- app/Core/Database/Connection.php
+++ app/Core/Database/Connection.php
@@ -1,1 +1,38 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\Core\Database;
+
+use PDO;
+
+final class Connection
+{
+    private PDO $pdo;
+
+    public function __construct(array $config)
+    {
+        $dsn = sprintf(
+            'mysql:host=%s;port=%s;dbname=%s;charset=%s',
+            $config['host'],
+            $config['port'],
+            $config['database'],
+            $config['charset']
+        );
+
+        $this->pdo = new PDO(
+            $dsn,
+            $config['username'],
+            $config['password'],
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]
+        );
+    }
+
+    public function pdo(): PDO
+    {
+        return $this->pdo;
+    }
+}
